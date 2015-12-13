7ce45f0afc9dab68241000ea2677b2ce8895e4f6
Prince Kumar <princekumar@air.local>
2015-12-13 09:18:21 +0530

    Initial Commit

---
+++ swift-ios9-coredata-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-ios9-coredata-sample
+//
+//  Created by Prince Kumar on 13/12/15.
+//  Copyright © 2015 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
